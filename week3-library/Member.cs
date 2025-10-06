352b0286ce48a3cbb95159c08828667cdb75efd3
sarim <syed-sa@ulster.ac.uk>
2025-10-06 16:16:25 +0100

    Updated code with a new member class

---
+++ week3-library/Member.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_library
+{
+    internal class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+    
+        // Public properties
+        public int MemberId
+        {
+            get { return memberId; }
+            set { memberId = value; } // Private setter makes it read-only
+        }
+        public string Name
+        {
+            get { return name; }  // get method
+            set { name = value; } // set method
+        }
+        public string Address
+        {
+            get { return address; }  // get method
+            set { address = value; } // set method
+        }
+        public int Phone
+        {
+            get { return phone; }  // get method
+            set { phone = value; } // set method
+        }
+
+        // Constructor for new member
+        public Member(int memberId, string name, string address, int phone)
+        {
+            this.MemberId = memberId;
+            this.Name = name;
+            this.Address = address;
+            this.Phone = phone;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {MemberId}");
+            Console.WriteLine($"Member name: {Name}");
+            Console.WriteLine($"Member address: {Address}");
+            Console.WriteLine($"Member phone number: {Phone}");
+            Console.WriteLine();
+        }
+    } 
+}
--- week3-library/Program.cs
+++ week3-library/Program.cs
@@ -6,12 +6,19 @@ class Program
     {
         // Creating a new instance of the Book class
         Book book = new Book("C# for beginners", "Bill Gates", "12345678");
-
-        // New instance called 'book1'
         Book book1 = new Book("C# Methods and Classes", "Microsoft", "55667778");
 
         // Output the book information to the console
         book.DisplayInfo();
         book1.DisplayInfo();
+
+        // Create new instances of the Member class
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
+
     }
 }
