86c2be8dcebd3a585ff7f56f6b8f78ea320c2327
sarim <syed-sa@ulster.ac.uk>
2025-10-06 15:58:18 +0100

    Updated code with variables and properties.

--- week3-library/Book.cs
+++ week3-library/Book.cs
@@ -8,9 +8,28 @@ namespace week3_library
 {
     internal class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        // Private variables
+        private string title;
+        private string author;
+        private string isbn;
+
+        // Properties to allow access to private variables
+        public string Title
+        {
+            get { return title; } // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
 
         // Create a constructor for object 'Book'
         public Book(string bookTitle, string bookAuthor, string bookISBN)
@@ -27,6 +46,5 @@ namespace week3_library
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
         }
-
     }
 }
